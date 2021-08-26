9e99926128e183bc3a54906872b2e0ce8710b8a8
pypas <pypasofar@gmail.com>
2021-08-26 14:14:01 -0300

    converts image to ascii art

--- script.py
+++ script.py
@@ -1,10 +1,42 @@
 from PIL import Image
-from numpy import asarray
+import numpy as np
 
-im = Image.open("image.jpg")
+img = Image.open("image.jpg")
 print("Successfully loaded image!")
-#im.show()
-print(f"Image size: {im.height} x {im.width}")
+print(f"Image size: {img.height} x {img.width}")
 
-im_as_array = asarray(im)
+basewidth = 250
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+
+# Turn image into a 3d array
+im_as_array = np.asarray(img)
 print(im_as_array.shape)
+
+def formula_lightness(x):
+    return (int(np.min(x)) + int(np.max(x)))/2
+def formula_average(x):
+    return np.mean(x)
+def formula_luminosity(x):
+    return 0.21*x[0] + 0.72*x[1] + 0.07*x[2]
+
+# Converting RGB matrix into a brightness matrix
+brighteness_matrix = np.apply_along_axis(formula_average, 2, im_as_array)
+print(brighteness_matrix.shape)
+
+# Convert this brightness matrix to an ASCII character matrix
+ascii_brightness_scale = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+idx_range = len(ascii_brightness_scale) - 1
+
+def convert_brightness_ascii(x):
+    return [ascii_brightness_scale[round(brightness/255*idx_range)] for brightness in x]
+
+ascii_matrix = np.apply_along_axis(convert_brightness_ascii, 1, brighteness_matrix)
+for line in ascii_matrix:
+    new_line = []
+    for item in line:
+        new_line.append(item)
+        new_line.append(item)
+        new_line.append(item)
+    print("".join(new_line))
