87343f08a00d0ef602802c5e909bc5ee8e6b4b73
pypas <pypasofar@gmail.com>
2021-08-27 09:03:03 -0300

    updates README

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 # ASCII Art
 > A simple program that turns images into ASCII-art
 
+
+## Instructions:
+
+1. Upload an image to the `img` directory.
+
+2. Update configs at the beginning of the script.
+- `IMAGE_IN_PATH`: path to original image
+- `IMAGE_OUT_PATH`: path to generated image
+- `STRATEGY`: strategy to map RGB values to brightness *(default = 1)*
+    - 1 = Average
+    - 2 = Luminosity
+    - 3 = Lightness
+- `OUTPUT`: whether to create a new image or print to terminal *(default = 1)*
+    - 1 = Image
+    - 2 = Terminal
+- `FONT_SIZE`: font size of ASCII characters *(default = 8)*
+- `FONT`: font family *(default = DejaVuSansMono)*
+- `MAX_WIDTH`: maximum width allowed for conversion *(default = 400)*
+    - If the image's width is greater than this value, image size is reduced
+
+3. Run the script.
+```py
+python3 script.py
+```
+
+## Example
+Original Image:
+
+![original_image](img/original_image.jpg)
+
+ASCII Image:
+
+![ascii_image](img/ascii_image.png)
+
+## Credits
 Inspired by: [Robert Heaton's Programming Projects For Advanced Beginners](https://robertheaton.com/2018/06/12/programming-projects-for-advanced-beginners-ascii-art/)
--- script.py
+++ script.py
@@ -7,22 +7,26 @@ fonts = [
 ] 
 
 # Configs
+IMAGE_IN_PATH = "img/original_image.jpg"
+IMAGE_OUT_PATH = "img/ascii_image.png"
 STRATEGY = 1 # 1 = Average, 2 = Luminosity, 3 = Lightness
-BASEWIDTH = 150
+OUTPUT = 1 # 1 = Image, 2 = Terminal
 FONT_SIZE = 8
-FONT = fonts[0]
+FONT = fonts[2]
+MAX_WIDTH = 400
 
 # Loads image
-img = Image.open("img/original_image.jpg")
+img = Image.open(IMAGE_IN_PATH)
 print("Successfully loaded image!")
 height = img.height
 width = img.width
 print(f"Original image size: {width} x {height}")
 
-# Reduces image size
-wpercent = (BASEWIDTH/float(img.size[0]))
-hsize = int((float(img.size[1])*float(wpercent)))
-img = img.resize((BASEWIDTH,hsize), Image.ANTIALIAS)
+# Reduces image size if width is greater than MAX_WIDTH
+if (width > MAX_WIDTH):
+    wpercent = (MAX_WIDTH/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((MAX_WIDTH,hsize), Image.ANTIALIAS)
 
 # Turns image into a 3d array
 im_as_array = np.asarray(img)
@@ -49,19 +53,18 @@ def convert_brightness_ascii(x):
     return [ascii_brightness_scale[round(brightness/255*idx_range)] for brightness in x]
 
 ascii_matrix = np.apply_along_axis(convert_brightness_ascii, 1, brighteness_matrix)
-full_txt = ""
-for line in ascii_matrix:
-    new_line = []
-    for item in line:
-        new_line += [item]*3
-    full_txt += "".join(new_line) + "\n"
 
-write_image_width = 2100
-write_image_height = 2000
-print(f"Best write image size: {write_image_width} x {write_image_height}")
-im = Image.new('RGB', (write_image_width, write_image_height), color = (0,0,0))
-fnt = ImageFont.truetype(fonts[0], FONT_SIZE) 
-ImageDraw.Draw(im).text((0,0), full_txt, font=fnt, fill=(255,255,255))
+ascii_text = '\n'.join(''.join(char*3 for char in line) for line in ascii_matrix)
 
-im = im.resize((width,height), Image.ANTIALIAS)
-im.save("img/ascii_image.png", "PNG")
+if OUTPUT == 1:
+    # Converts text to image
+    fnt = ImageFont.truetype(fonts[0], FONT_SIZE)
+    write_image_width, write_image_height = ImageDraw.Draw(Image.new('RGB', (1, 1))).textsize(ascii_text, font=fnt)
+    im = Image.new('RGB', (write_image_width, write_image_height), color = (0,0,0))
+    ImageDraw.Draw(im).text((0,0), ascii_text, font=fnt, fill=(255,255,255))
+
+    # Resizes image and saves to file
+    im = im.resize((width,height), Image.ANTIALIAS)
+    im.save(IMAGE_OUT_PATH, "PNG")
+else:
+    print(ascii_text)
